cade79a23f9b141be5b530197efa2e3afba4cafc
Arato <contact@arato.mg>
2022-05-05 15:59:41 +0300

    5-5 15:59

--- nbt.sql
+++ nbt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 22 fév. 2022 à 10:36
+-- Généré le : jeu. 05 mai 2022 à 14:58
 -- Version du serveur : 10.4.21-MariaDB
 -- Version de PHP : 8.0.11
 
@@ -20,6 +20,23 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `nbt`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `assigner`
+--
+
+CREATE TABLE `assigner` (
+  `id_as` int(11) NOT NULL,
+  `id_camion` int(11) NOT NULL,
+  `id_pneu` int(11) NOT NULL,
+  `etat_pneu` varchar(10) NOT NULL,
+  `date_d` date NOT NULL,
+  `kilo_d` int(11) NOT NULL,
+  `date_fin` date NOT NULL,
+  `kilo_fin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -44,6 +61,28 @@ INSERT INTO `autre` (`id_autre`, `date_autre`, `designation`, `montant`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `autre_conso`
+--
+
+CREATE TABLE `autre_conso` (
+  `id_conso` int(11) NOT NULL,
+  `date_conso` date NOT NULL,
+  `designation` varchar(200) NOT NULL,
+  `prix_gasoil` int(11) NOT NULL,
+  `gasoil_litre` int(11) NOT NULL,
+  `id_carte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `autre_conso`
+--
+
+INSERT INTO `autre_conso` (`id_conso`, `date_conso`, `designation`, `prix_gasoil`, `gasoil_litre`, `id_carte`) VALUES
+(1, '2022-04-30', 'test', 4000, 232, 6);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `avoir`
 --
 
@@ -58,13 +97,9 @@ CREATE TABLE `avoir` (
 --
 
 INSERT INTO `avoir` (`id_avoir`, `id_facture`, `id_voyage`) VALUES
-(28, 16, 8),
-(29, 16, 9),
-(32, 18, 12),
-(33, 18, 13),
-(34, 18, 6),
-(41, 22, 7),
-(42, 22, 11);
+(43, 23, 14),
+(44, 24, 16),
+(45, 24, 15);
 
 -- --------------------------------------------------------
 
@@ -76,19 +111,18 @@ CREATE TABLE `camion` (
   `id_camion` int(11) NOT NULL,
   `matricule` varchar(10) NOT NULL,
   `marque` varchar(50) NOT NULL,
-  `modele` varchar(13) DEFAULT NULL
+  `genre` varchar(255) NOT NULL,
+  `modele` varchar(13) DEFAULT NULL,
+  `norme` int(11) NOT NULL,
+  `type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `camion`
 --
 
-INSERT INTO `camion` (`id_camion`, `matricule`, `marque`, `modele`) VALUES
-(3, '3472TBG', 'MERC', NULL),
-(4, '2870TBH', 'MERC', NULL),
-(5, '9426TBF', 'MERC', NULL),
-(6, '5287TBK', 'MERC', NULL),
-(7, '2243TBJ', 'MERC', NULL);
+INSERT INTO `camion` (`id_camion`, `matricule`, `marque`, `genre`, `modele`, `norme`, `type`) VALUES
+(10, '3472 TBG', 'RENAULT', 'Prenium', '250', 54, 'Plateau');
 
 -- --------------------------------------------------------
 
@@ -99,17 +133,19 @@ INSERT INTO `camion` (`id_camion`, `matricule`, `marque`, `modele`) VALUES
 CREATE TABLE `carte_gasoil` (
   `id_carte` int(11) NOT NULL,
   `libelle` varchar(100) NOT NULL,
-  `capacite` int(6) DEFAULT NULL
+  `plafond` int(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `carte_gasoil`
 --
 
-INSERT INTO `carte_gasoil` (`id_carte`, `libelle`, `capacite`) VALUES
-(2, 'CARTE TOTAL 59888', 3426),
+INSERT INTO `carte_gasoil` (`id_carte`, `libelle`, `plafond`) VALUES
+(2, 'CARTE TOTAL 59888', 3430),
 (3, 'CARTE TOTAL 59890', NULL),
-(4, 'CARTE TOTAL 59884', NULL);
+(4, 'CARTE TOTAL 59889', NULL),
+(5, 'CARTE TOTAL 59891', NULL),
+(6, 'CARTE TOTAL 59887', NULL);
 
 -- --------------------------------------------------------
 
@@ -121,8 +157,8 @@ CREATE TABLE `chauffeur` (
   `id_chauffeur` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(100) DEFAULT NULL,
-  `cin` text NOT NULL,
-  `adresse` varchar(100) DEFAULT NULL,
+  `permis` text NOT NULL,
+  `date_permis` date DEFAULT NULL,
   `tel` varchar(13) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -130,10 +166,8 @@ CREATE TABLE `chauffeur` (
 -- Déchargement des données de la table `chauffeur`
 --
 
-INSERT INTO `chauffeur` (`id_chauffeur`, `nom`, `prenom`, `cin`, `adresse`, `tel`) VALUES
-(4, 'LITA', NULL, '00000000', '0210 viirfi', NULL),
-(5, 'RODDY', NULL, '00000000', '0210 viirfi', NULL),
-(6, 'OLIN', NULL, '00000000', NULL, '0000000');
+INSERT INTO `chauffeur` (`id_chauffeur`, `nom`, `prenom`, `permis`, `date_permis`, `tel`) VALUES
+(7, 'NDRIANALINIRINA', 'Donné', '528363T', '2022-04-21', '0349752848');
 
 -- --------------------------------------------------------
 
@@ -145,17 +179,46 @@ CREATE TABLE `chek` (
   `id_chek` int(11) NOT NULL,
   `numero` varchar(20) NOT NULL,
   `date_chek` date NOT NULL,
-  `montant_chek` int(11) NOT NULL,
-  `id_facture` int(11) NOT NULL
+  `designation` varchar(100) NOT NULL,
+  `montant_chek` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `chek`
 --
 
-INSERT INTO `chek` (`id_chek`, `numero`, `date_chek`, `montant_chek`, `id_facture`) VALUES
-(2, '1', '2022-02-24', 10843, 22),
-(3, '23', '2022-02-17', 1152000, 16);
+INSERT INTO `chek` (`id_chek`, `numero`, `date_chek`, `designation`, `montant_chek`) VALUES
+(4, '1', '2022-04-09', '', 0),
+(5, '1', '2022-04-09', '', 2000),
+(6, '1', '2022-04-16', 'test', 2000),
+(7, '1212', '2022-05-05', '', 20000),
+(8, '1212', '2022-05-05', '', 20000),
+(9, '1212', '2022-05-05', '', 20000);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cheque`
+--
+
+CREATE TABLE `cheque` (
+  `id_cheque` int(11) NOT NULL,
+  `id_facture` int(11) NOT NULL,
+  `date_cheque` date NOT NULL,
+  `numero` int(11) NOT NULL,
+  `montant` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `cheque`
+--
+
+INSERT INTO `cheque` (`id_cheque`, `id_facture`, `date_cheque`, `numero`, `montant`) VALUES
+(1, 0, '0000-00-00', 1212, 0),
+(2, 0, '0000-00-00', 1212, 0),
+(3, 0, '0000-00-00', 1212, 0),
+(4, 0, '0000-00-00', 1212, 0),
+(5, 0, '0000-00-00', 1212, 0);
 
 -- --------------------------------------------------------
 
@@ -185,7 +248,10 @@ INSERT INTO `depense` (`id_depense`, `id_voyage`, `montant_gasoil`, `gasoil_litr
 (9, 10, 6, 232, 20000, 2000, 455),
 (10, 11, 6, 232, 20000, 2000, 455),
 (11, 12, 200000, 232, NULL, NULL, 45000),
-(12, 13, 20000, 232, NULL, NULL, 450000);
+(12, 13, 20000, 232, NULL, NULL, 450000),
+(13, 14, 23, 232, 20000, 2000, 500),
+(14, 15, 6, 40, NULL, 2, 455),
+(15, 16, 6, 40, NULL, 4, 455);
 
 -- --------------------------------------------------------
 
@@ -216,7 +282,10 @@ INSERT INTO `detail` (`id_detail`, `id_voyage`, `tonnage_aller`, `retour`, `tonn
 (5, 9, 1, 'az', 22, 3, 300000, 23, 'sd'),
 (6, 11, 1, 'fd', 1, 3, 3000, 267, 'df'),
 (7, 12, 3, NULL, NULL, 2, 300000, 250, 'TC exemple'),
-(8, 13, 2, NULL, NULL, 1, 300000, 250, 'TC exemple');
+(8, 13, 2, NULL, NULL, 1, 300000, 250, 'TC exemple'),
+(9, 14, 4, 'test1', 10, 4, 2800, 256, 'TEST'),
+(10, 15, 9, 'az', 1, 0, 0, NULL, NULL),
+(11, 16, 7, 'sss', 9, 0, 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -243,9 +312,8 @@ CREATE TABLE `facture` (
 --
 
 INSERT INTO `facture` (`id_facture`, `num_fac`, `type`, `client`, `nif`, `stat`, `total`, `tva`, `total_final`, `date_fact`, `date_echeance`) VALUES
-(16, '1', 'import', 'AAAE', '1', '1', 960000, 192000, 1152000, '2022-01-23', '2022-01-21'),
-(18, '1TNT', 'import', 'ABM', '0000000', '0000000', 900222, 180044, 1080266, '2022-01-22', '2022-02-25'),
-(22, '1TNT', 'export', 'AAAsd', '0000000', '0000000', 9036, 1807, 10843, '2022-02-17', NULL);
+(23, '1', 'export', 'AAA', '0000000', '0000000', 11200, 2240, 13440, '2022-04-29', '2022-04-21'),
+(24, '1TNT', 'import', 'AAAE', '0000000', '0000000', 12, 4, 16, '2022-05-05', NULL);
 
 -- --------------------------------------------------------
 
@@ -312,14 +380,58 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `piece`
+--
+
+CREATE TABLE `piece` (
+  `id_piece` int(11) NOT NULL,
+  `date_piece` date NOT NULL,
+  `designation` varchar(255) NOT NULL,
+  `prix` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `piece`
+--
+
+INSERT INTO `piece` (`id_piece`, `date_piece`, `designation`, `prix`) VALUES
+(1, '2022-04-09', 'test', 100),
+(2, '2022-05-03', 'test1', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pneu`
+--
+
+CREATE TABLE `pneu` (
+  `id_pneu` int(11) NOT NULL,
+  `numero` varchar(30) NOT NULL,
+  `reference` varchar(20) NOT NULL,
+  `marque` varchar(40) NOT NULL,
+  `prix_pneu` int(11) NOT NULL,
+  `date_pneu` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `pneu`
+--
+
+INSERT INTO `pneu` (`id_pneu`, `numero`, `reference`, `marque`, `prix_pneu`, `date_pneu`) VALUES
+(2, '1', '200/LL/22,5', 'miclin', 20000, '0000-00-00'),
+(4, '1212', '200/LL/22,5', 'MERCede', 100, '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `recouvrement`
 --
 
 CREATE TABLE `recouvrement` (
   `id_recouvrement` int(11) NOT NULL,
   `id_facture` int(11) NOT NULL,
-  `date_payement` date NOT NULL,
-  `montant_payement` int(11) NOT NULL
+  `date_payement` date DEFAULT NULL,
+  `montant_payement` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -327,9 +439,28 @@ CREATE TABLE `recouvrement` (
 --
 
 INSERT INTO `recouvrement` (`id_recouvrement`, `id_facture`, `date_payement`, `montant_payement`) VALUES
-(4, 18, '2022-01-29', 1080266),
-(9, 22, '2022-02-19', 10843),
-(10, 16, '2022-02-20', 1152000);
+(2, 23, '2022-05-05', 20000);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transit`
+--
+
+CREATE TABLE `transit` (
+  `id_transit` int(11) NOT NULL,
+  `transit` varchar(255) NOT NULL,
+  `client` varchar(255) NOT NULL,
+  `nif` int(11) NOT NULL,
+  `stat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `transit`
+--
+
+INSERT INTO `transit` (`id_transit`, `transit`, `client`, `nif`, `stat`) VALUES
+(2, 'ARIVA', 'NEWDAN', 1, 12);
 
 -- --------------------------------------------------------
 
@@ -361,37 +492,52 @@ CREATE TABLE `voyage` (
   `id_voyage` int(11) NOT NULL,
   `date_voyage` date NOT NULL,
   `transit` varchar(100) DEFAULT NULL,
-  `client` varchar(255) DEFAULT NULL,
   `id_camion` int(11) NOT NULL,
   `id_chauffeur` int(11) NOT NULL,
   `com` int(11) DEFAULT NULL,
   `montant` int(11) NOT NULL,
-  `id_carte` int(11) NOT NULL
+  `id_carte` int(11) NOT NULL,
+  `bl` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `voyage`
 --
 
-INSERT INTO `voyage` (`id_voyage`, `date_voyage`, `transit`, `client`, `id_camion`, `id_chauffeur`, `com`, `montant`, `id_carte`) VALUES
-(6, '2013-02-19', 'BB', 'AAA', 4, 5, 200, 222, 3),
-(7, '2022-02-18', 'BB', 'AAAsd', 4, 5, 20000, 36, 4),
-(8, '2022-01-19', 'BB', 'AAA', 3, 5, 200, 60000, 2),
-(9, '2022-01-19', 'BB', 'AAAsd', 3, 5, 200, 900000, 2),
-(10, '2022-01-21', 'BB', 'AAA', 4, 6, 200, 3000, 3),
-(11, '2022-01-21', 'BB', 'AAA', 4, 6, 200, 9000, 3),
-(12, '2022-01-22', 'ABM', NULL, 4, 4, 20000, 600000, 3),
-(13, '2022-01-22', 'ABM', NULL, 4, 5, 20000, 300000, 2);
+INSERT INTO `voyage` (`id_voyage`, `date_voyage`, `transit`, `id_camion`, `id_chauffeur`, `com`, `montant`, `id_carte`, `bl`) VALUES
+(6, '2013-02-19', 'BB', 4, 5, 200, 222, 3, 0),
+(7, '2022-02-18', 'BB', 4, 5, 20000, 36, 4, 0),
+(8, '2022-01-19', 'BB', 3, 5, 200, 60000, 2, 0),
+(9, '2022-01-19', 'BB', 3, 5, 200, 900000, 2, 0),
+(10, '2022-01-21', 'BB', 4, 6, 200, 3000, 3, 0),
+(11, '2022-01-21', 'BB', 4, 6, 200, 9000, 3, 0),
+(12, '2022-01-22', 'ABM', 4, 4, 20000, 600000, 3, 0),
+(13, '2022-01-22', 'ABM', 4, 5, 20000, 300000, 2, 0),
+(14, '2022-04-09', 'test', 10, 7, 20000, 11200, 4, 0),
+(15, '2022-05-13', 'ARIVA', 10, 7, 200, 20000, 3, 1333),
+(16, '2022-05-25', 'ARIVA', 10, 7, 20000, 20000, 2, 12);
 
 --
 -- Index pour les tables déchargées
 --
 
 --
+-- Index pour la table `assigner`
+--
+ALTER TABLE `assigner`
+  ADD PRIMARY KEY (`id_as`);
+
+--
 -- Index pour la table `autre`
 --
 ALTER TABLE `autre`
   ADD PRIMARY KEY (`id_autre`);
+
+--
+-- Index pour la table `autre_conso`
+--
+ALTER TABLE `autre_conso`
+  ADD PRIMARY KEY (`id_conso`);
 
 --
 -- Index pour la table `avoir`
@@ -422,6 +568,12 @@ ALTER TABLE `chauffeur`
 --
 ALTER TABLE `chek`
   ADD PRIMARY KEY (`id_chek`);
+
+--
+-- Index pour la table `cheque`
+--
+ALTER TABLE `cheque`
+  ADD PRIMARY KEY (`id_cheque`);
 
 --
 -- Index pour la table `depense`
@@ -463,10 +615,28 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Index pour la table `piece`
+--
+ALTER TABLE `piece`
+  ADD PRIMARY KEY (`id_piece`);
+
+--
+-- Index pour la table `pneu`
+--
+ALTER TABLE `pneu`
+  ADD PRIMARY KEY (`id_pneu`);
+
+--
 -- Index pour la table `recouvrement`
 --
 ALTER TABLE `recouvrement`
   ADD PRIMARY KEY (`id_recouvrement`);
+
+--
+-- Index pour la table `transit`
+--
+ALTER TABLE `transit`
+  ADD PRIMARY KEY (`id_transit`);
 
 --
 -- Index pour la table `users`
@@ -485,58 +655,76 @@ ALTER TABLE `voyage`
 --
 
 --
+-- AUTO_INCREMENT pour la table `assigner`
+--
+ALTER TABLE `assigner`
+  MODIFY `id_as` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `autre`
 --
 ALTER TABLE `autre`
   MODIFY `id_autre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT pour la table `autre_conso`
+--
+ALTER TABLE `autre_conso`
+  MODIFY `id_conso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT pour la table `avoir`
 --
 ALTER TABLE `avoir`
-  MODIFY `id_avoir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id_avoir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT pour la table `camion`
 --
 ALTER TABLE `camion`
-  MODIFY `id_camion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_camion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `carte_gasoil`
 --
 ALTER TABLE `carte_gasoil`
-  MODIFY `id_carte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_carte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `chauffeur`
 --
 ALTER TABLE `chauffeur`
-  MODIFY `id_chauffeur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_chauffeur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `chek`
 --
 ALTER TABLE `chek`
-  MODIFY `id_chek` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_chek` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `cheque`
+--
+ALTER TABLE `cheque`
+  MODIFY `id_cheque` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `depense`
 --
 ALTER TABLE `depense`
-  MODIFY `id_depense` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_depense` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT pour la table `detail`
 --
 ALTER TABLE `detail`
-  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `facture`
 --
 ALTER TABLE `facture`
-  MODIFY `id_facture` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_facture` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT pour la table `migrations`
@@ -551,10 +739,28 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pour la table `piece`
+--
+ALTER TABLE `piece`
+  MODIFY `id_piece` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `pneu`
+--
+ALTER TABLE `pneu`
+  MODIFY `id_pneu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT pour la table `recouvrement`
 --
 ALTER TABLE `recouvrement`
-  MODIFY `id_recouvrement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_recouvrement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `transit`
+--
+ALTER TABLE `transit`
+  MODIFY `id_transit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `users`
@@ -566,7 +772,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `voyage`
 --
 ALTER TABLE `voyage`
-  MODIFY `id_voyage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_voyage` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
